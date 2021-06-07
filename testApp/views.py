925c7b0c21ba64590b133b92a16d0cbcac04799c
shkimm5189 <shkimm5189@gmail.com>
2021-06-07 10:46:51 +0900

    restFramework 적용

--- testApp/views.py
+++ testApp/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(req):
+
     return HttpResponse("index Page");
 
 def test(req):
     return HttpResponse("test")
 
 def user(requ):
+
     return HttpResponse("user")
