e14105f9e9a781fb0ae260690c5c4f929dd167ab
shkimm5189 <shkimm5189@gmail.com>
2021-06-07 11:48:35 +0900

    DB 연동 및 확인 테스트 완료

--- testApp/urls.py
+++ testApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index()),
-    path('', views.test()),
-    path('', views.user())
+    path('', views.index),
+    path('', views.test),
+    path('', views.user)
 
 ]
--- testApp/views.py
+++ testApp/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
-
+from django.shortcuts import render
 def index(req):
-
-    return HttpResponse("index Page");
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(req, 'pages/question_list.html',context)
 
 def test(req):
     return HttpResponse("test")
