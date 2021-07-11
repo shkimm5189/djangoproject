94e00425688073770e75b220354a66af8b130deb
shkimm5189 <shkimm5189@gmail.com>
2021-07-11 23:44:48 +0900

    Refact : change DB setting

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k)(j@tsi#7slo)usho(=&k!tx3&je&65w&f+ids+z(@w3v!mdo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- my_dbsetting.py
+++ my_dbsetting.py
@@ -2,9 +2,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 사용할 DB
         'NAME': 'django_test',                 # database 이름
-        'USER': 'django',                        # 접속 계정
-        'PASSWORD': 'test',              # 접속 계정 비밀번호
-        'HOST': 'localhost',                    # DB 주소
+        'USER': 'django_test',                        # 접속 계정
+        'PASSWORD': '1234',              # 접속 계정 비밀번호
+        'HOST': 'db-server',                    # DB 주소
         'PORT': '3306'                         # port
     }
 }
